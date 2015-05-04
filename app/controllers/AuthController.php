f03ff8d507b2cc049a4c8e5472bf1a17a5b818d2
sky <sky@sky-Inspiron-N5110.(none)>
2015-05-04 08:11:03 +0300

    soc net login

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -66,7 +66,7 @@ class AuthController extends BaseController {
             // Send a request with it
             $result = json_decode( $fb->request( '/me' ), true );
             if(isset($result['id']) && !empty($result['id'])){
-                var_dump($create);
+                var_dump($create); exit;
                 if(!empty($create)){
                     return Redirect::to('/')->with('socId',$result['id'])->with('socNetwork','facebook');
                 } else {
