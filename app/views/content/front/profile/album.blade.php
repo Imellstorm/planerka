59c5520ad1dbb97e238bf92bf1e21d4b095920d8
sky <sky@sky-Inspiron-N5110.(none)>
2015-05-19 11:02:40 +0300

    share image

--- app/views/content/front/profile/album.blade.php
+++ app/views/content/front/profile/album.blade.php
@@ -37,9 +37,9 @@
 								@endif
 								<div class="share">
 									<h4>Поделиться</h4>
-									<a href="#null" class="facebook"></a>
-									<a href="#null" class="vk"></a>
-									<a href="#null" class="twitter"></a>
+									<a class="vk" onclick="Share.vkontakte('','TITLE','{{ URL::to('/').'/'.$image->thumb_big }}','DESC')"></a>
+									<a class="facebook" onclick="Share.facebook('URL','TITLE','IMG_PATH','DESC')"></a>
+									<a class="twitter" onclick="Share.twitter('URL','TITLE')"></a>
 								</div>
 							</div>
 						</div>
@@ -131,6 +131,51 @@
 			})
 		})
 	})
+
+	Share = {
+	    vkontakte: function(purl, ptitle, pimg, text) {
+	        url  = 'http://vkontakte.ru/share.php?';
+	        url += 'url='          + encodeURIComponent(purl);
+	        url += '&title='       + encodeURIComponent(ptitle);
+	        url += '&description=' + encodeURIComponent(text);
+	        url += '&image='       + encodeURIComponent(pimg);
+	        url += '&noparse=true';
+	        Share.popup(url);
+	    },
+	    odnoklassniki: function(purl, text) {
+	        url  = 'http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1';
+	        url += '&st.comments=' + encodeURIComponent(text);
+	        url += '&st._surl='    + encodeURIComponent(purl);
+	        Share.popup(url);
+	    },
+	    facebook: function(purl, ptitle, pimg, text) {
+	        url  = 'http://www.facebook.com/sharer.php?s=100';
+	        url += '&p[title]='     + encodeURIComponent(ptitle);
+	        url += '&p[summary]='   + encodeURIComponent(text);
+	        url += '&p[url]='       + encodeURIComponent(purl);
+	        url += '&p[images][0]=' + encodeURIComponent(pimg);
+	        Share.popup(url);
+	    },
+	    twitter: function(purl, ptitle) {
+	        url  = 'http://twitter.com/share?';
+	        url += 'text='      + encodeURIComponent(ptitle);
+	        url += '&url='      + encodeURIComponent(purl);
+	        url += '&counturl=' + encodeURIComponent(purl);
+	        Share.popup(url);
+	    },
+	    mailru: function(purl, ptitle, pimg, text) {
+	        url  = 'http://connect.mail.ru/share?';
+	        url += 'url='          + encodeURIComponent(purl);
+	        url += '&title='       + encodeURIComponent(ptitle);
+	        url += '&description=' + encodeURIComponent(text);
+	        url += '&imageurl='    + encodeURIComponent(pimg);
+	        Share.popup(url)
+	    },
+
+	    popup: function(url) {
+	        window.open(url,'','toolbar=0,status=0,width=626,height=436');
+	    }
+	};
 
 </script>
 @stop
