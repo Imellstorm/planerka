efcd7c482bd47b52a5f54367acad1bafbb43f226
sky <sky@sky-Inspiron-N5110.(none)>
2015-05-19 12:11:11 +0300

    share image

--- app/views/content/front/profile/album.blade.php
+++ app/views/content/front/profile/album.blade.php
@@ -149,11 +149,12 @@
 	        Share.popup(url);
 	    },
 	    facebook: function(purl, ptitle, pimg, text) {
-	        url  = 'http://www.facebook.com/sharer.php?s=100';
-	        url += '&p[title]='     + encodeURIComponent(ptitle);
-	        url += '&p[summary]='   + encodeURIComponent(text);
-	        url += '&p[url]='       + encodeURIComponent(purl);
-	        url += '&p[images][0]=' + encodeURIComponent(pimg);
+	        url  = 'http://www.facebook.com/sharer.php?src=sp&version=2';
+	        //url += '&p[title]='     + encodeURIComponent(ptitle);
+	        //url += '&p[summary]='   + encodeURIComponent(text);
+	        //url += '&p[url]='       + encodeURIComponent(purl);
+	       // url += '&p[images][0]=' + encodeURIComponent(pimg);
+	       	url += '&picture=' + encodeURIComponent(pimg);
 	        Share.popup(url);
 	    },
 	    twitter: function(purl, ptitle) {
