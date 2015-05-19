9de446df8109ea2487c12ab1c500a398a51e8737
sky <sky@sky-Inspiron-N5110.(none)>
2015-05-19 12:18:13 +0300

    share image

--- app/views/content/front/profile/album.blade.php
+++ app/views/content/front/profile/album.blade.php
@@ -149,12 +149,11 @@
 	        Share.popup(url);
 	    },
 	    facebook: function(purl, ptitle, pimg, text) {
-	        url  = 'http://www.facebook.com/sharer.php?src=sp&version=2';
-	        //url += '&p[title]='     + encodeURIComponent(ptitle);
-	        //url += '&p[summary]='   + encodeURIComponent(text);
+	        url  = 'http://www.facebook.com/sharer.php?s=100';
+	        // url += '&p[title]='     + encodeURIComponent(ptitle);
+	        // url += '&p[summary]='   + encodeURIComponent(text);
 	        url += '&p[url]='       + encodeURIComponent(purl);
 	        // url += '&p[images][0]=' + encodeURIComponent(pimg);
-	       	//url += '&picture=' + encodeURIComponent(pimg);
 	        Share.popup(url);
 	    },
 	    twitter: function(purl, ptitle) {
