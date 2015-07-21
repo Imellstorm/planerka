02a61342eb0cd6e96dff236b8c5449d02e4387aa
sky <sky@sky-Inspiron-N5110.(none)>
2015-07-21 10:31:58 +0300

    fix

--- app/views/content/front/index.blade.php
+++ app/views/content/front/index.blade.php
@@ -43,7 +43,7 @@
                     <ul class="theme-list">
                         @foreach($blogThemes as $item)
                             <li>
-                                <div class="title">{{ $item->name }}</div>
+                            <div class="title"><a href="/blog/theme/{{ $item->id }}">{{ $item->name }}</a></div>
                                 <div class="comment">{{ $item->postscount }}</div>
                                 <div class="date">{{ Common_helper::translateDate(strtotime($item->created_at)) }}</div>
                             </li>
