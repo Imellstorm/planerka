d1afbcec68a73230e66d18a47e2dfc587c40ccae
sky <sky@sky-Inspiron-N5110.(none)>
2015-07-30 15:55:02 +0300

    fix

--- app/models/Projectmessages.php
+++ app/models/Projectmessages.php
@@ -7,15 +7,13 @@ class Projectmessages extends \Eloquent {
     public function getProjectMessagesByUser($projectId,$userId,$toArray=false){
     	$this->userId = $userId;
     	if($toArray) DB::setFetchMode(PDO::FETCH_ASSOC);
+    	$tablePrefix = DB::getTablePrefix();
 		$result = DB::table($this->table)
 					->select('users.alias','users.online','users.role_id','user_info.*',$this->table.'.*')
 					->leftjoin('user_info','user_info.user_id','=',$this->table.'.from_user')
 					->leftjoin('users','users.id','=',$this->table.'.from_user')
 					->where($this->table.'.project_id',$projectId)
-					->where(function($query){
-							$query->where('project_messages.from_user',$this->userId)
-								  ->orWhere('project_messages.to_user',$this->userId);
-					})
+					->whereRaw('('.$tablePrefix.$this->table.'.from_user = '.$this->userId.' or '.$tablePrefix.$this->table.'.to_user = '.$this->userId.')')
 					->orderby($this->table.'.id','DESC');
 		if($toArray){ 
 			$result = $result->get();
